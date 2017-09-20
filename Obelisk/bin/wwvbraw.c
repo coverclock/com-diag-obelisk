1ea634e32f2ca973f444463e19f5e9bde5f3acf5
Chip Overclock <coverclock@diag.com>
2017-09-20 08:14:27 -0600

    [1] Added code to reduce jitter in polling loop.

--- Obelisk/bin/wwvbraw.c
+++ Obelisk/bin/wwvbraw.c
@@ -14,8 +14,8 @@
 #include <assert.h>
 #include "com/diag/diminuto/diminuto_pin.h"
 #include "com/diag/diminuto/diminuto_delay.h"
-#include "com/diag/diminuto/diminuto_cue.h"
 #include "com/diag/diminuto/diminuto_time.h"
+#include "com/diag/diminuto/diminuto_cue.h"
 
 static const int PIN_OUT_P1 = 23; /* output, radio enable, active low. */
 static const int PIN_IN_T = 24; /* input, modulated pulse, active high */
@@ -25,15 +25,18 @@ int main(int argc, char ** argv)
     int rc = -1;
     FILE * pin_out_p1_fp = (FILE *)0;
     FILE * pin_in_t_fp = (FILE *)0;
-    diminuto_sticks_t frequency = -1;
-    diminuto_ticks_t delay = -1;
-    diminuto_sticks_t slack = -1;
+    diminuto_sticks_t ticks_frequency = -1;
+    diminuto_ticks_t ticks_delay = -1;
+    diminuto_sticks_t ticks_slack = -1;
+    diminuto_sticks_t ticks_before = -1;
+    diminuto_sticks_t ticks_after = -1;
+    diminuto_ticks_t ticks_elapsed = -1;
     diminuto_cue_state_t cue = { 0 };
     int edge_raw = -1;
     int edge_before = -1;
     int edge_after = -1;
-    diminuto_sticks_t ticks_before = -1;
-    diminuto_sticks_t ticks_after = -1;
+    int cycles_count = -1;
+    int cycles_total = -1;
 
     /*
     ** Configure P1 output and T input pins.
@@ -59,56 +62,70 @@ int main(int argc, char ** argv)
     ** Toggle P1 output pin (active low).
     */
 
-    frequency = diminuto_delay_frequency();
-    assert(frequency > 0);
-    delay = frequency / 2;
+    ticks_frequency = diminuto_delay_frequency();
+    assert(ticks_frequency > 0);
+    ticks_delay = ticks_frequency / 2;
 
     rc = diminuto_pin_set(pin_out_p1_fp);
     assert(rc == 0);
 
-    slack = diminuto_delay(delay, 0);
-    assert(slack == 0);
+    ticks_slack = diminuto_delay(ticks_delay, 0);
+    assert(ticks_slack == 0);
 
     rc = diminuto_pin_clear(pin_out_p1_fp);
     assert(rc == 0);
 
-    slack = diminuto_delay(delay, 0);
-    assert(slack == 0);
+    ticks_slack = diminuto_delay(ticks_delay, 0);
+    assert(ticks_slack == 0);
 
     /*
     ** Poll T input pin changes.
     */
 
-    delay = frequency / 100;
-
-    frequency = diminuto_time_frequency();
-    assert(frequency > 0);
-
     diminuto_cue_init(&cue, 0);
 
+    ticks_delay = ticks_frequency / 100;
+
+    cycles_count = 0;
+    cycles_total = 0;
+
     while (!0) {
+
+        ticks_before = diminuto_time_elapsed();
+        assert(ticks_before >= 0);
 
         edge_raw = diminuto_pin_get(pin_in_t_fp);
         assert(edge_raw >= 0);
 
         edge_after = diminuto_cue_debounce(&cue, edge_raw);
 
-        ticks_after = diminuto_time_elapsed();
-        assert(ticks_after >= 0);
-
-        if (edge_before < 0) {
-            /* Do nothing: initialization. */
-        } else if (edge_before == edge_after) {
-            /* Do nothing: should never happen. */
+        if (edge_before == 0) {
+            /* Do nothing. */
+        } else if (edge_after == 0) {
+            cycles_total = cycles_count;
+            cycles_count = 0;
         } else {
-            printf("T %d %d %lld\n", edge_before, edge_after, (ticks_after - ticks_before) * 1000 / frequency);
+            ++cycles_count;
+        }
+
+        if (cycles_total > 0) {
+            printf("T %d %d %d\n", edge_before, edge_after, cycles_total);
+            cycles_total = 0;
         }
 
         edge_before = edge_after;
-        ticks_before = ticks_after;
 
-        slack = diminuto_delay(delay, 0);
-        assert(slack == 0);
+        ticks_after = diminuto_time_elapsed();
+        assert(ticks_after >= 0);
+
+        assert(ticks_after >= ticks_before);
+        ticks_elapsed = ticks_after - ticks_before;
+
+        if (ticks_elapsed < ticks_delay) {
+            ticks_slack = diminuto_delay(ticks_delay - ticks_elapsed, 0);
+            assert(ticks_slack == 0);
+        }
+
     }
 
     /*
