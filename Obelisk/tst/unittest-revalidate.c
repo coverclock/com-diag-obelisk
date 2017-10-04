ed4a5cb6c59049e0e384467601264425c62ca4aa
Chip Overclock <coverclock@diag.com>
2017-10-04 10:23:56 -0600

    [1] Checkpoint.

--- Obelisk/tst/unittest-revalidate.c
+++ Obelisk/tst/unittest-revalidate.c
@@ -23,19 +23,27 @@ int main(int argc, char ** argv)
     diminuto_core_enable();
 
 	{
+        int count = -1;
         struct tm time = { 0 };
+        int leap = -1;
         int days = -1;
+        int gmonth = -1;
+        int gday = -1;
         int rc = -1;
 
         TEST();
 
+        count = 0;
+
+        time.tm_wday = 5; /* 2016-01-01 */
         for (int year = 2016; year <= 2017; ++year) {
+            leap = (((year % 4) == 0) && ((year % 400) != 0));
             time.tm_year = year - 1900;
             time.tm_yday = 0;
             for (int month = 1; month <= 12; ++month) {
                 switch (month) {
                 case 2:
-                    days = (((year % 4) == 0) && ((year % 400) != 0)) ? 29 : 28;
+                    days = leap ? 29 : 28;
                     break;
                 case 4:
                 case 6:
@@ -49,15 +57,32 @@ int main(int argc, char ** argv)
                 }
                 time.tm_mon = month - 1;
                 for (time.tm_mday = 1; time.tm_mday <= days; ++time.tm_mday) {
-                    time.tm_yday += 1;
+                    rc = obelisk_zeller(time.tm_year + 1900, time.tm_mon + 1, time.tm_mday);
+                    if (rc != time.tm_wday) {
+                        CHECKPOINT("%04d-%02d-%02d %d (%d)\n",
+                            time.tm_year + 1900, time.tm_mon + 1, time.tm_mday,
+                            time.tm_wday, rc);
+                    }
+                    ASSERT(rc == time.tm_wday);
+                    gmonth = -1;
+                    gday = -1;
+                    rc = obelisk_julian2gregorian(time.tm_yday + 1, leap, &gmonth, &gday);
+                    if (rc < 0) {
+                        CHECKPOINT("%04d-%02d-%02d %d (%d) %d %d\n",
+                            time.tm_year + 1900, time.tm_mon + 1, time.tm_mday,
+                            time.tm_yday, rc, gmonth, gday);
+                    }
+                    ASSERT(rc >= 0);
+                    ASSERT(gmonth == (time.tm_mon + 1));
+                    ASSERT(gday == time.tm_mday);
                     for (time.tm_hour = 0; time.tm_hour <= 23; ++time.tm_hour) {
                         for (time.tm_min = 0; time.tm_min <= 59; ++time.tm_min) {
                             for (time.tm_sec = 0; time.tm_sec <= 60; ++time.tm_sec) {
                                 for (time.tm_isdst = -1; time.tm_isdst <= 1; ++time.tm_isdst) {
+                                    ++count;
                                     rc = obelisk_revalidate(&time);
                                     if (rc < 0) {
-                                        printf("%04d-%02d-%02dT%02d:%02d:%02d %d %d %d (%d)\n",
-                
+                                        CHECKPOINT("%04d-%02d-%02dT%02d:%02d:%02d %d %d %d (%d)\n",
                                             time.tm_year + 1900, time.tm_mon + 1, time.tm_mday,
                                             time.tm_hour, time.tm_min, time.tm_sec,
                                             time.tm_wday, time.tm_yday, time.tm_isdst,
@@ -68,9 +93,15 @@ int main(int argc, char ** argv)
                             }
                         }
                     }
+                    time.tm_wday = (time.tm_wday + 1) % 7;
+                    time.tm_yday += 1;
                 }
             }
         }
+
+        CHECKPOINT("count %d\n", count);
+        fflush(stdout);
+        ASSERT(count == (366 + 367) * 24 * 60 * 60 * 3);
 
         STATUS();
     }
